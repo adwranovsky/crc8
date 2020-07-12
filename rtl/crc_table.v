24f691511115c7c5f1ae83ca5c85563b517af99f
Alexander Wranovsky <alex@wranovsky.org>
2020-07-12 16:11:04 -0500

    Wrote the crc8 module

--- rtl/crc_table.v
+++ rtl/crc_table.v
@@ -8,13 +8,12 @@ module crc_table #(
 ) (
     input wire clk_i,
     input wire [7:0] addr_i,
-    output wire value
+    output wire [7:0] value
 );
     // Instantiate a ROM
     reg [7:0] rom [0:255];
-    reg [7:0] rom_addr;
     reg [7:0] rom_data_out;
-    reg rom_en;
+    wire rom_en;
     assign rom_en = 1;
     always @(posedge clk_i) begin
         if (rom_en) begin
@@ -24,20 +23,21 @@ module crc_table #(
     assign value = rom_data_out;
 
     // Fill the ROM with the CRC table for this polynomial
-    integer i, bit_num, actual_polynomial, table_entry;
+    integer i, bit_num;
+    reg [8:0] actual_polynomial, table_entry;
     initial begin
         // Add the implied 1 to the beginning of the polynomial
-        actual_polynomial = {2'b01, POLYNOMIAL[7:0]};
+        actual_polynomial = {1'b1, POLYNOMIAL[7:0]};
         for (i = 0; i < 256; i = i+1) begin
             // For each possible value in the table entry, divide it by the polynomial and then store it in the table
-            table_entry = i;
+            table_entry = i[8:0];
             for (bit_num = 0; bit_num < 8; bit_num = bit_num+1) begin
-                table_entry = {table_entry, 0};
-                if (table_entry[7]) begin
+                table_entry = {table_entry[7:0], 1'b0};
+                if (table_entry[8]) begin
                     table_entry = table_entry ^ actual_polynomial;
                 end
             end
-            rom[i] = table_entry;
+            rom[i] = table_entry[7:0];
         end
     end
 endmodule
